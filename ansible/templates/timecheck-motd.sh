f94c384dbc522274c9af12d643e5dcf261f78ce5
Matthew Brooks <Logicwax@gmail.com>
2023-02-08 20:33:40 -0800

    motd timecheck slight changes

--- ansible/templates/timecheck-motd.sh
+++ ansible/templates/timecheck-motd.sh
@@ -7,8 +7,10 @@ BRED='\033[1;31m'
 GREEN='\033[0;32m'
 
 clear
-echo "This machine's clock is currently set to: $(date) "$GREEN"($(TZ=America/Los_Angeles date))"$OFF"\n\nIf this is not correct and you plan on performing any PGP operations (key creation, signing, encryption) then you should correct the system time to the current time in order to avoid time drift issues.\n\nTo change the system time (along with system hardware clock) using UTC:\n"
+echo "This machine's clock is currently set to: $(date) "$GREEN"($(TZ=America/Los_Angeles date))"$OFF"\n\nIf this is not correct and you plan on performing any PGP operations (key creation, signing, encryption) then you should correct the system time to the current time in order to avoid time drift issues.\n\nTo change the system time (along with system hardware clock):\n"
 
-echo $RED"sudo timedatectl set-time \""$BRED"YYYY-MM-DD HH:MM:SS"$OFF$RED"\""$OFF"\n"
+echo "1) First set the timezone with the command "$RED"sudo timedatectl set-timezone "$BRED"America/Los_Angeles"$OFF"  (For a list of timezones use "$RED"timedatectl list-timezones"$OFF")\n"
 
-echo "(or if you prefer to set in your current timezone, you may issue the command "$RED"sudo timedatectl set-timezone "$BRED"America/Los_Angeles"$OFF" first.\n\n"
+echo "2) Set time with the command "$RED"sudo timedatectl set-time \""$BRED"YYYY-MM-DD HH:MM:SS"$OFF$RED"\""$OFF" (default is in UTC if timezone from step #1 isn't set)\n\n"
+
+echo "Run the command "$RED"help"$OFF" to repeat this message.\n"
